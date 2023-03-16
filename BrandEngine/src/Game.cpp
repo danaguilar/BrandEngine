470b04cf4df302c437e052f90f12513cf3420ae3
Daniel Aguilar <danaguilar6@gmail.com>
2023-03-15 22:25:32 -0500

    Added true fullscreen and GPU rendering to window

--- BrandEngine/src/Game.cpp
+++ BrandEngine/src/Game.cpp
@@ -19,11 +19,14 @@ void Game::Initialize()
   {
     std::cerr << "SDL failed to initialize" << std::endl;
   }
+  
+  int displayWidth = 800;
+  int displayHeight = 600;
 
   window = SDL_CreateWindow(
     NULL,                                                   // Title
     SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,         // Window X, Y Position
-    800, 600,                                               // WIndow height, width
+    displayWidth, displayHeight,                                               // WIndow height, width
     SDL_WINDOW_BORDERLESS                                   // Special Flags
   );
   if(!window)
@@ -32,15 +35,17 @@ void Game::Initialize()
   }
 
   renderer = SDL_CreateRenderer(
-    window,      // Created window which new rendered should attach to
-    -1,          // Specfic driver to attach initialize with (-1 is default)                                                                                          
-    0            // Special Flags
+    window,                                                 // Created window which new rendered should attach to
+    -1,                                                     // Specfic driver to attach initialize with (-1 is default)                                                                                          
+    SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC    // Special Flags
   );
 
   if(!renderer)
   {
     std::cerr << "Renderer failed to create" << std::endl;
   }
+
+  SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
 
   isRunning = true;
 }
@@ -54,30 +59,31 @@ void Game::Destroy()
 
 void Game::Run()
 {
-  while (true)
+  while (isRunning)
   {
     ProcessInput();
     Update();
-    Run();
+    Render();
   }
 }
 
 void Game::ProcessInput()
 {
   SDL_Event sdlEvent;
-  SDL_PollEvent(&sdlEvent); // Poll event every frame
-  switch (sdlEvent.type)
+  while(SDL_PollEvent(&sdlEvent)) // Poll event every frame
   {
-    case SDL_QUIT:          // Stop running game if window is closed
-      isRunning = false;
-      break;
-    case SDL_KEYDOWN:
-      if (sdlEvent.key.keysym.sym == SDLK_ESCAPE)  // Stop running game if esc key is pressed
-      {
+    switch (sdlEvent.type)
+    {
+      case SDL_QUIT:          // Stop running game if window is closed
         isRunning = false;
-      }
+        break;
+      case SDL_KEYDOWN:
+        if (sdlEvent.key.keysym.sym == SDLK_ESCAPE)  // Stop running game if esc key is pressed
+        {
+          isRunning = false;
+        }
+    }
   }
-
 }
 
 void Game::Update()
@@ -86,4 +92,8 @@ void Game::Update()
 
 void Game::Render()
 {
+  SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
+  SDL_RenderClear(renderer);
+
+  SDL_RenderPresent(renderer);
 }
