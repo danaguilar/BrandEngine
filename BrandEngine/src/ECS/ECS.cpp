5a0f88c0c4580274c458342fa602784f73f5badf
Daniel Aguilar <danaguilar6@gmail.com>
2023-03-23 12:32:45 -0500

    Added components pools, entity vectors, and system maps to Registry

--- BrandEngine/src/ECS/ECS.cpp
+++ BrandEngine/src/ECS/ECS.cpp
@@ -4,3 +4,29 @@ int Entity::GetID() const
 {
   return id;
 }
+
+void System::AddEntity(Entity entity)
+{
+  entities.push_back(entity);
+}
+
+void System::RemoveEntity(Entity entity)
+{
+  entities.erase(
+    std::remove_if(
+      entities.begin(), entities.end(),
+      [&entity](Entity other) { return entity == other; }
+    ),
+    entities.end()
+  );
+}
+
+std::vector<Entity> System::GetEntities() const
+{
+  return entities;
+}
+
+const Signature& System::GetComponentSignature() const
+{
+  return componentSignature;
+}
--- BrandEngine/src/ECS/ECS.h
+++ BrandEngine/src/ECS/ECS.h
@@ -3,6 +3,8 @@
 
 #include <bitset>
 #include <vector>
+#include <unordered_map>
+#include <typeindex>
 
 unsigned int const MAX_COMPONENTS = 32;
 typedef std::bitset<MAX_COMPONENTS> Signature;
@@ -30,6 +32,7 @@ class Entity
   public:
     Entity(int id): id(id) {};
     int GetID() const;
+    bool operator ==(const Entity& other) const { return id == other.GetID(); };
 };
 
 class System
@@ -43,13 +46,55 @@ class System
     ~System() = default;
     void AddEntity(Entity);
     void RemoveEntity(Entity);
-    std::vector<Entity>& GetEntities() const;
-    Signature& GetComponentSignature() const;
+    std::vector<Entity> GetEntities() const;
+    const Signature& GetComponentSignature() const;
     template<typename TComponent> void RegisterComponent();
 };
 
+class IPool
+{
+  virtual ~IPool();
+};
+
+template<typename T>
+class Pool : public IPool
+{
+  private:
+    std::vector<T> data;
+  
+  public:
+    Pool(int n = 100) { resize(n); };
+    ~Pool();
+    void Add(T object) { data.push_back(object); }
+    void Clear() { data.clear()  }
+    void Resize(int n) { data.resize(n); }
+    void Set(int index, T object) { data[index] = object; }
+    T& Get(int index) { return static_cast<T&>(data[index]); }
+    T& operator [](unsigned int index) { return date[index]; }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////
+// REGISTRY
+////////////////////////////////////////////////////////////////////////////////////////
+// Manages the creation and destruction of enitites and adds systems and components
+////////////////////////////////////////////////////////////////////////////////////////
 class Registry
 {
+  private:
+    int numEntities = 0;
+
+    // Stores components of the same type into pools. Each pool is ordered by the entity of that component
+    // componentPool[componentID]
+    // pool[entityID]
+    std::vector<IPool*> componentPools;
+
+    // Signature of each entity
+    // [entityID]
+    std::vector<Signature> entityComponentSignatures;
+      
+    std::unordered_map<std::type_index, System*> systems;
+
+    
 
 };
 
